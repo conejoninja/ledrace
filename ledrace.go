3f092e7680d5af28d3c1c5fffe2315fadb65581e
Daniel Esteban <conejo@theninjabunny.com>
2019-04-20 12:24:45 +0200

    add configuration mode

--- ledrace.go
+++ ledrace.go
@@ -30,6 +30,7 @@ type Player struct {
 	speed    float32
 	position float32
 	color    color.RGBA
+	loop     uint8
 }
 
 var players [2]Player
@@ -61,6 +62,13 @@ func main() {
 		track.gravity[i] = 127
 	}
 
+	setRamp(12, 90, 100, 110)
+
+	// If at start, player 0 is pressed, enter configuration mode
+	if !players[0].button.Get() {
+		showGravity()
+	}
+
 	players[0].color = red
 	players[1].color = green
 
@@ -89,13 +97,26 @@ func main() {
 			players[p].speed -= players[p].speed * FRICTION
 			players[p].position += players[p].speed
 
-			if players[p].position > LAPS*TRACKLENGHT {
-				finishRace(p)
-				break
+			if players[p].position > TRACKLENGHT*float32(players[p].loop) {
+				players[p].loop++
 			}
+
 		}
 
 		paintTrack()
+
+		maxPosition := players[0].position
+		winner := uint8(0)
+		for p := uint8(1); p < PLAYERS; p++ {
+			if players[p].position > maxPosition {
+				maxPosition = players[p].position
+				winner = p
+			}
+		}
+		if maxPosition > LAPS*TRACKLENGHT {
+			finishRace(winner)
+		}
+
 		time.Sleep(10 * time.Millisecond)
 	}
 }
@@ -105,7 +126,9 @@ func paintTrack() {
 		track.leds[i] = black
 	}
 	for p := uint8(0); p < PLAYERS; p++ {
-		track.leds[uint32(players[p].position)%TRACKLENGHT] = players[p].color
+		for l := uint8(0); l < players[p].loop; l++ {
+			track.leds[(uint32(players[p].position)-uint32(l))%TRACKLENGHT] = players[p].color
+		}
 	}
 	track.ws.WriteColors(track.leds)
 }
@@ -159,4 +182,50 @@ func resetPlayers() {
 		players[p].position = 0
 		players[p].pressed = false
 	}
+}
+func setRamp(height uint8, start uint32, middle uint32, end uint32) {
+	for i := uint32(0); i < (middle - start); i++ {
+		track.gravity[start+i] = uint8(127 - float32(i)*float32(height)/float32(middle-start))
+	}
+	track.gravity[middle] = 127
+	for i := uint32(0); i < (end - middle); i++ {
+		track.gravity[middle+i+1] = uint8(127 + float32(height)*(1-float32(i)/float32(middle-start)))
+	}
+}
+
+func showGravity() {
+	for i := 0; i < TRACKLENGHT; i++ {
+		grav := track.gravity[i]
+		r := uint8(2)
+		g := uint8(2)
+		if grav < 127 {
+			r = 2 * (127 - grav)
+		}
+		if grav > 127 {
+			g = (grav - 127) * 2
+		}
+		track.leds[i] = color.RGBA{r, g, 0, 255}
+	}
+	track.ws.WriteColors(track.leds)
+	longpress := 0
+	onoff := false
+	for {
+		if !players[0].button.Get() {
+			longpress++
+			if longpress > 20 {
+				break
+			}
+		} else {
+			longpress = 0
+		}
+		if onoff {
+			players[0].led.Low()
+		} else {
+			players[0].led.High()
+		}
+		onoff = !onoff
+		time.Sleep(100 * time.Millisecond)
+	}
+	players[0].led.Low()
+	time.Sleep(2 * time.Second)
 }
